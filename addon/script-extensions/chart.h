e952f7f19f283cf571037f95a43ac8b605fa5999
Jonny <you@example.com>
2026-05-28 16:39:53 +0200

    feat: enhance chart functionality with dynamic axis scales and improved layout

--- addon/script-extensions/chart.h
+++ addon/script-extensions/chart.h
@@ -4,6 +4,8 @@
 struct Chart {
   lv_obj_t *           chart;
   lv_chart_series_t *  ser;
+  lv_obj_t *           x_scale;
+  lv_obj_t *           y_scale;
 };
 
 static Chart draw_chart(
@@ -13,13 +15,12 @@ static Chart draw_chart(
   u_int max_y_value,
   u_int x_grid_lines,
   u_int y_grid_lines,
-  void (*fptr)(lv_event_t* e),
   lv_obj_t * parent = NULL
 )
 {
   lv_obj_t * container;
   if(parent == NULL)
-    container = lv_obj_create(lv_scr_act());
+    container = lv_obj_create(lv_screen_active());
   else
     container = lv_obj_create(parent);
 
@@ -28,12 +29,22 @@ static Chart draw_chart(
   lv_obj_set_style_border_width(container, 0, LV_PART_MAIN);
   lv_obj_set_style_pad_all(container, 14, LV_PART_MAIN);
 
+  int pad = 14;
+  int inner_width = width - (pad * 2);
+  int inner_height = height - (pad * 2);
+
+  int y_scale_width = 45;
+  int x_scale_height = 30;
+
+  int chart_width = inner_width - y_scale_width;
+  int chart_height = inner_height - x_scale_height;
+
   Chart instance;
   instance.chart = lv_chart_create(container);
   lv_obj_align(instance.chart, LV_ALIGN_TOP_RIGHT, 0, 0);
-  lv_obj_set_size(instance.chart, width - 55, height - 40);
+  lv_obj_set_size(instance.chart, chart_width, chart_height);
   lv_obj_set_style_bg_color(instance.chart, BACKGROUND_COLOR, LV_PART_MAIN);
-  lv_obj_set_style_size(instance.chart, 0, LV_PART_INDICATOR);
+  lv_obj_set_style_size(instance.chart, 0, 0, LV_PART_INDICATOR);
   lv_obj_set_style_line_color(instance.chart, PRIMARY_COLOR, LV_STATE_DEFAULT);
   lv_obj_set_style_border_width(instance.chart, 0, LV_PART_MAIN);
 
@@ -43,9 +54,61 @@ static Chart draw_chart(
   lv_chart_set_update_mode(instance.chart, LV_CHART_UPDATE_MODE_SHIFT);
   lv_chart_set_range(instance.chart, LV_CHART_AXIS_PRIMARY_X, 0, max_x_value);
   lv_chart_set_range(instance.chart, LV_CHART_AXIS_PRIMARY_Y, 0, max_y_value);
-  lv_chart_set_axis_tick(instance.chart, LV_CHART_AXIS_PRIMARY_Y, 0, 0, y_grid_lines, 1, true, 50);
-  lv_chart_set_axis_tick(instance.chart, LV_CHART_AXIS_PRIMARY_X, 0, 0, x_grid_lines, 2, true, 50);
-  lv_obj_add_event_cb(instance.chart, fptr, LV_EVENT_DRAW_PART_BEGIN, NULL);
+
+  // Y-axis Scale on the left
+  instance.y_scale = lv_scale_create(container);
+  lv_scale_set_mode(instance.y_scale, LV_SCALE_MODE_VERTICAL_LEFT);
+  lv_obj_set_size(instance.y_scale, y_scale_width, chart_height);
+  lv_obj_align(instance.y_scale, LV_ALIGN_TOP_LEFT, 0, 0);
+  lv_scale_set_total_tick_count(instance.y_scale, y_grid_lines);
+  lv_scale_set_major_tick_every(instance.y_scale, 1);
+  lv_scale_set_label_show(instance.y_scale, true);
+  lv_scale_set_range(instance.y_scale, 0, max_y_value);
+  
+  // Style Y-axis Scale
+  lv_obj_set_style_line_color(instance.y_scale, PRIMARY_COLOR, LV_PART_MAIN);
+  lv_obj_set_style_line_color(instance.y_scale, PRIMARY_COLOR, LV_PART_ITEMS);
+  lv_obj_set_style_line_color(instance.y_scale, PRIMARY_COLOR, LV_PART_INDICATOR);
+  lv_obj_set_style_text_color(instance.y_scale, TEXT_COLOR, LV_PART_INDICATOR);
+
+  // Generate Y-axis labels dynamically (bottom-to-top)
+  char ** y_labels = new char*[y_grid_lines + 1];
+  for (u_int i = 0; i < y_grid_lines; i++) {
+    int val = (max_y_value * i) / (y_grid_lines - 1);
+    y_labels[i] = new char[16];
+    lv_snprintf(y_labels[i], 16, "%d W", val);
+  }
+  y_labels[y_grid_lines] = NULL;
+  lv_scale_set_text_src(instance.y_scale, (const char **)y_labels);
+
+  // X-axis Scale at the bottom
+  instance.x_scale = lv_scale_create(container);
+  lv_scale_set_mode(instance.x_scale, LV_SCALE_MODE_HORIZONTAL_BOTTOM);
+  lv_obj_set_size(instance.x_scale, chart_width, x_scale_height);
+  lv_obj_align(instance.x_scale, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+  lv_scale_set_total_tick_count(instance.x_scale, x_grid_lines);
+  lv_scale_set_major_tick_every(instance.x_scale, 1);
+  lv_scale_set_label_show(instance.x_scale, true);
+
+  // Style X-axis Scale
+  lv_obj_set_style_line_color(instance.x_scale, PRIMARY_COLOR, LV_PART_MAIN);
+  lv_obj_set_style_line_color(instance.x_scale, PRIMARY_COLOR, LV_PART_ITEMS);
+  lv_obj_set_style_line_color(instance.x_scale, PRIMARY_COLOR, LV_PART_INDICATOR);
+  lv_obj_set_style_text_color(instance.x_scale, TEXT_COLOR, LV_PART_INDICATOR);
+
+  // Generate X-axis labels dynamically
+  char ** x_labels = new char*[x_grid_lines + 1];
+  for (u_int i = 0; i < x_grid_lines; i++) {
+    x_labels[i] = new char[16];
+    uint value = 24 - (i * 2);
+    if (value == 0) {
+      lv_snprintf(x_labels[i], 16, "now");
+    } else {
+      lv_snprintf(x_labels[i], 16, "-%d", value);
+    }
+  }
+  x_labels[x_grid_lines] = NULL;
+  lv_scale_set_text_src(instance.x_scale, (const char **)x_labels);
 
   instance.ser = lv_chart_add_series(instance.chart, lv_palette_main(LV_PALETTE_ORANGE), LV_CHART_AXIS_PRIMARY_Y);
 
--- addon/solar-chart/solar-chart.h
+++ addon/solar-chart/solar-chart.h
@@ -5,30 +5,7 @@
 static Chart chart_home_instance;
 static Chart chart_solar_instance;
 
-static void DrawCallbackEvent(lv_event_t* e)
-{
-    lv_obj_draw_part_dsc_t* dsc = lv_event_get_draw_part_dsc(e);
-
-    if (!lv_obj_draw_part_check_type(dsc, &lv_chart_class, LV_CHART_DRAW_PART_TICK_LABEL))
-        return;
-    if (dsc->text == NULL)
-        return;
-
-    if (dsc->id == LV_CHART_AXIS_PRIMARY_X)
-    {
-        uint value = 24 - ((dsc->value) * 2);
-        if(value == 0)
-          lv_snprintf(dsc->text, dsc->text_length, "now");
-        else
-          lv_snprintf(dsc->text, dsc->text_length, "-%d", value);
-    }
-    else if (dsc->id == LV_CHART_AXIS_PRIMARY_Y)
-    {
-        lv_snprintf(dsc->text, dsc->text_length, "%d%", dsc->value);
-    }
-}
-
 static Chart draw_solar_chart(lv_obj_t * parent = NULL)
 {
-  return draw_chart(398, 324, 288, 800, 13, 9, DrawCallbackEvent, parent);
+  return draw_chart(398, 324, 288, 800, 13, 9, parent);
 }
